20d642c7b15192efd3041b216566e60ee2e19b8f
Toommo23 <142088873+Tommo-23@users.noreply.github.com>
2024-08-24 12:50:30 +0200

    hh

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 import os
-import telebot
-from dotenv import load_dotenv
+import asyncio
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
+from telegram.error import NetworkError
 
-# Load environment variables
-load_dotenv()
+TOKEN = os.getenv("TOKEN")
 
-# Replace 'TELEGRAM_BOT_TOKEN' with the token you received from BotFather
-TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-bot = telebot.TeleBot(TOKEN)
+CHAT_DESTINAZIONE = "@TuttoModding"  # Canale di destinazione
 
-@bot.message_handler(commands=['start', 'hello'])
-def send_welcome(message):
-    bot.reply_to(message, "Hello! I'm a simple Telegram bot.")
+CANALI_MONITORATI = {
+    '@garnet_updates': 'rn13pro5g / PocoX6',
+    '@PocoF6GlobalUpdate': 'POCO F6',
+    '@Redmi10CUpdates': 'Redmi 10c',    
+    '@setupTommo23': 'test',    
+}
 
-@bot.message_handler(func=lambda msg: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+async def start(update: Update, context: CallbackContext) -> None:
+    await update.message.reply_text('my job is to forward the new roms of your device.')
 
-bot.polling()
+async def inoltra_messaggio(update: Update, context: CallbackContext) -> None:
+    if update.message and update.message.chat.username in CANALI_MONITORATI:
+        try:
+            await update.message.forward(chat_id=CHAT_DESTINAZIONE)
+        except NetworkError as e:
+            print(f"Network error occurred: {e}. Retrying in 5 seconds...")
+            await asyncio.sleep(5)
+            await update.message.forward(chat_id=CHAT_DESTINAZIONE)
+    else:
+        print("L'aggiornamento ricevuto non proviene da un canale monitorato.")
+
+def main() -> None:
+    application = Application.builder().token(TOKEN).build()
+
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(MessageHandler(filters.ChatType.CHANNEL, inoltra_messaggio))
+
+    application.run_polling()
+
+if __name__ == '__main__':
+    main()
